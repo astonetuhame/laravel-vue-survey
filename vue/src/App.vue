91751ace6d5b0c5c19c14958b01f466dc47aad0b
Astone Tuhame <astonetuhame@gmail.com>
2022-03-01 18:53:26 +0300

    Update App.vue

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -16,17 +16,16 @@ export default {
 </script>
 
 <template>
-
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <router-view></router-view>
 </template>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
