7d5f03757a6220d4a317e47af28a52a36cbb1298
Astone Tuhame <astonetuhame@gmail.com>
2022-03-08 19:21:00 +0300

    Update Surveys.vue

--- vue/src/views/Surveys.vue
+++ vue/src/views/Surveys.vue
@@ -1,26 +1,8 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-   <header class="bg-white shadow">
-      <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold text-gray-900">Surveys</h1>
-      </div>
-    <main>
-      <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-        <!-- Replace with your content -->
-        <div class="px-4 py-6 sm:px-0">
-          Surveys
-        </div>
-        <!-- /End replace -->
-      </div>
-    </main>
-    </header>
+   <PageComponent title="Surveys">Content</PageComponent>
 </template>
 
-<script>
- 
-export default {
-   
-  
-   
-}
+<script setup>
+import PageComponent from "../components/PageComponent.vue"
 </script>
